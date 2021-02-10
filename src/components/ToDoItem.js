fc9175beb31fb111a082862a7f1609cba733c7b3
Marko-Gregurovic <marko.gregurovic0304@gmail.com>
2021-02-10 20:27:20 +0100

    Phase 5

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 function ToDoItem(props){
     return (
         <div className="form-control bg-dark text-light">
-            <input style={{backgroundColor: "#20FF20", borderColor: "#20FF20"}} className="form-check-input btn-primary" type="checkbox" checked={props.item.completed}/>
+            <input onChange={() => console.log("ticked")} style={{backgroundColor: "#20FF20", borderColor: "#20FF20"}} className="form-check-input btn-primary" type="checkbox" checked={props.item.completed}/>
             <label className="form-check-label"> {`${props.item.text}`}</label><br/>
         </div>
     );
