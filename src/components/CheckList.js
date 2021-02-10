aa390fbd1b34ad6a57ab15128acfa5753a3d3aac
Marko-Gregurovic <marko.gregurovic0304@gmail.com>
2021-02-10 19:54:59 +0100

    Phase 4

--- src/components/CheckList.js
+++ src/components/CheckList.js
@@ -2,18 +2,34 @@ import React from 'react';
 
 import ToDoItem from './ToDoItem'
 import toDoItems from '../toDoItems';
-import { NavItem } from 'react-bootstrap';
 
 const items = toDoItems.map(item => {
     return <ToDoItem key={item.id} item={item} />
 });
 
-function CheckList(){
-    return (
-        <div className="container form-group">
-            {items}
-        </div>
-    );
+// function CheckList(){
+//     return (
+//         <div className="container form-group">
+//             {items}
+//         </div>
+//     );
+// }
+
+class CheckList extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            items: items
+        };
+    }
+    
+    render() {
+        return (
+            <div className="container form-group">
+                {this.state.items}
+            </div>
+        );
+    }
 }
 
 export default CheckList;
